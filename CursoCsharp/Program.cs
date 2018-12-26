fd812ae970193651a7e2cea20338c9d6673d2ade
Ilton Batista <iltonbatista2018@gmail.com>
2018-12-26 01:27:52 -0200

    Calculadora de somar

--- CursoCsharp/Program.cs
+++ CursoCsharp/Program.cs
@@ -4,31 +4,26 @@ namespace estudar
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            double num1, num2;
+            string tmp;
+
+            Console.WriteLine("Insira um valor: "); //imprimir
+            tmp = Console.ReadLine(); //leitura de string
+            num1 = int.Parse(tmp); //conversão para o tipo na variavel que eu atribuir
 
 
+            Console.WriteLine("Insira mas um valor: "); //imprimir
+            tmp = Console.ReadLine(); //leitura de string
+            num2 = int.Parse(tmp); //conversão para o tipo na variavel que eu atribuir
 
-            //Converter String para inteiro
-            String s = "10";
-            int i;
+            double resultado;
+            resultado = num1 + num2; //atribui num1 e num2 a resultado para depois imprimir
 
-            i = int.Parse(s);
-
-            int x = i + 12;
-
-            Console.WriteLine(x);
-
-
-
-
-
-
+            Console.WriteLine("Resultado final: " + resultado);
 
             Console.ReadLine();
         }
-
-
     }
 }
