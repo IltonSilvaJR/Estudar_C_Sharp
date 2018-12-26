9e425b1a7e837635745022b432e0f77cbba95e4f
Ilton Batista <iltonbatista2018@gmail.com>
2018-12-26 10:29:23 -0200

    Mini calculadora

--- CursoCsharp/Program.cs
+++ CursoCsharp/Program.cs
@@ -18,10 +18,21 @@ namespace estudar
             tmp = Console.ReadLine(); //leitura de string
             num2 = int.Parse(tmp); //conversão para o tipo na variavel que eu atribuir
 
-            double resultado;
-            resultado = num1 + num2; //atribui num1 e num2 a resultado para depois imprimir
+            double resultado1;
+            resultado1 = num1 + num2; //atribui num1 e num2 a resultado para depois imprimir
+            Console.WriteLine("Soma: " + resultado1);
 
-            Console.WriteLine("Resultado final: " + resultado);
+            double resultado2;
+            resultado2 = num1 * num2; //atribui num1 e num2 a resultado para depois imprimir
+            Console.WriteLine("Multiplicação: " + resultado2);
+
+            double resultado3;
+            resultado3 = num1 / num2; //atribui num1 e num2 a resultado para depois imprimir     
+            Console.WriteLine("divisão: " + resultado3);
+
+            double resultado4;
+            resultado4 = num1 - num2; //atribui num1 e num2 a resultado para depois imprimir
+            Console.WriteLine("subtração: " + resultado4);
 
             Console.ReadLine();
         }
